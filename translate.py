6a6cc029698073e203c9958a31d897dd140b3016
SimranGitCode <simrann115@gmail.com>
2025-03-25 19:23:39 +0530

    Delete translate.py

--- translate.py
+++
@@ -1,6 +0,0 @@
-from googletrans import Translator
-
-def translate_caption(text, target_language):
-    translator = Translator()
-    translated_text = translator.translate(text, dest=target_language)
-    return translated_text.text
